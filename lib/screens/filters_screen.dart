cad6b4ac838be84887e8bbe7893dc22b3e8ceed4
stands30 <lancylotdsouza@gmail.com>
2021-06-27 15:46:12 +0530

    meals app

---
+++ lib/screens/filters_screen.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import '../widgets/main_drawer.dart';
+
+class FiltersScreen extends StatefulWidget {
+  static const routeName = '/filters';
+  final Function saveFilters;
+  final Map<String, bool> currentFilters;
+  const FiltersScreen(this.currentFilters, this.saveFilters);
+
+  @override
+  _FiltersScreenState createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  var _glutenFree = false;
+  var _vegetarian = false;
+  var _vegan = false;
+  var _lactoseFree = false;
+
+  @override
+  initState() {
+    _glutenFree = widget.currentFilters['glutten'];
+    _vegetarian = widget.currentFilters['vegeterian'];
+    _vegan = widget.currentFilters['vegan'];
+    _lactoseFree = widget.currentFilters['lactose'];
+    super.initState();
+  }
+
+  Widget _buildSwitchListStyle(String title, String description,
+      bool currentValue, Function updateValue) {
+    return SwitchListTile(
+      title: Text(title),
+      value: currentValue,
+      subtitle: Text(description),
+      onChanged: updateValue,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Filters'),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.save),
+              onPressed: () {
+                final selectedFilters = {
+                  'glutten': _glutenFree,
+                  'lactose': _lactoseFree,
+                  'vegeterian': _vegetarian,
+                  'vegan': _vegan,
+                };
+                print(selectedFilters);
+                widget.saveFilters(selectedFilters);
+              })
+        ],
+      ),
+      drawer: MainDrawer(),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Adjust your meal selection.',
+              style: Theme.of(context).textTheme.title,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                _buildSwitchListStyle('Gluten Free',
+                    'Only include gluten-free meals', _glutenFree, (newValue) {
+                  setState(() {
+                    _glutenFree = newValue;
+                  });
+                }),
+                _buildSwitchListStyle(
+                    'Lactose Free',
+                    'Only include lactose-free meals',
+                    _lactoseFree, (newValue) {
+                  setState(() {
+                    _lactoseFree = newValue;
+                  });
+                }),
+                _buildSwitchListStyle('Vegetarian Free',
+                    'Only include vegeterian meals', _vegetarian, (newValue) {
+                  setState(() {
+                    _vegetarian = newValue;
+                  });
+                }),
+                _buildSwitchListStyle(
+                    'Vegan Free', 'Only include vegan meals', _vegan,
+                    (newValue) {
+                  setState(() {
+                    _vegan = newValue;
+                  });
+                }),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/main_drawer.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import '../screens/filters_screen.dart';
+
+class MainDrawer extends StatelessWidget {
+  const MainDrawer();
+
+  Widget buildListStyle(String title, IconData icon, Function tapHandler) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        size: 26,
+      ),
+      title: Text(
+        title,
+        style: TextStyle(
+            fontFamily: 'RobotoCondensed',
+            fontSize: 24,
+            fontWeight: FontWeight.bold),
+      ),
+      onTap: tapHandler,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          Container(
+            height: 120,
+            width: double.infinity,
+            padding: EdgeInsets.all(20),
+            alignment: Alignment.centerLeft,
+            color: Theme.of(context).accentColor,
+            child: Text(
+              'Cooking Up',
+              style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                  fontSize: 20,
+                  color: Theme.of(context).primaryColor),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          buildListStyle('Meals', Icons.restaurant, () {
+            Navigator.of(context).pushReplacementNamed('/');
+          }),
+          buildListStyle('Filters', Icons.settings, () {
+            Navigator.of(context).pushReplacementNamed(FiltersScreen.routeName);
+          }),
+        ],
+      ),
+    );
+  }
+}
